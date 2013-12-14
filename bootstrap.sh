206867c316bff3d4896e9dbdf53d092b9c6c9d13
Doug Sheffer <doug.sheffer@researchsquare.com>
2013-12-14 18:05:57 -0500

    Added stashing to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 cd "$(dirname $0)"
 HERE="$(pwd)"
 
+STASH="$HERE/.stash/$(date +%s)"
+REVERT="$STASH/revert"
+
 for FILE in *; do
 
     [ "$FILE" == "$(basename $0)" ] && continue
@@ -10,8 +13,27 @@ for FILE in *; do
 
     SRC="$HERE/$FILE"
     DEST="$HOME/.$FILE"
-
     echo "Installing $DEST..."
+
+    # Move any existing files into the stash.
+    if [ ! -L "$DEST" ]; then
+        mkdir -p "$STASH"
+
+        STASHFILE="$STASH/.$FILE"
+        mv "$DEST" "$STASHFILE"
+        echo "Stashed existing file to $STASHFILE"
+
+        echo "# Revert .$FILE" >> "$REVERT"
+        echo "rm -f '$DEST'" >> "$REVERT"
+        echo "cp '$STASHFILE' '$DEST'" >> "$REVERT"
+        echo >> "$REVERT"
+    fi
+
+    # Link the new file into place.
     rm -f "$DEST" && ln -s "$SRC" "$DEST"
 
 done
+
+if [ -e "$REVERT" ]; then
+    echo "Revert log stored to $REVERT"
+fi
