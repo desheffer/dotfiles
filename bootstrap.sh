d11c85694a44e00adf9d2c24b57e797ae29c917d
Doug Sheffer <doug.sheffer@researchsquare.com>
2017-03-09 06:00:29 +0000

    Upgrade instead of just install

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,4 +52,4 @@ mkdir -p ~/.backup
 [ ! -f ~/.vim/autoload/plug.vim ] && \
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+vim +PlugUpdate +qall
