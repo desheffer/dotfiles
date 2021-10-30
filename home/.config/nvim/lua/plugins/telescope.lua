97e6a9272470641eb49494c114109dc347b88bbc
Doug Sheffer <desheffer@gmail.com>
2021-10-29 23:43:32 -0400

    Change telescope git_commits to git_bcommits

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -21,6 +21,6 @@ vim.api.nvim_set_keymap("n", "<Leader>fr", [[<Cmd>lua require("telescope.builtin
 vim.api.nvim_set_keymap("n", "<Leader>fw", [[<Cmd>lua require("telescope.builtin").grep_string()<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("v", "<Leader>fw", [[<Cmd>lua require("telescope.builtin").grep_string()<CR>]], {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<Leader>gc", [[<Cmd>lua require("telescope.builtin").git_commits()<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>gc", [[<Cmd>lua require("telescope.builtin").git_bcommits()<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>gf", [[<Cmd>lua require("telescope.builtin").git_files()<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>gs", [[<Cmd>lua require("telescope.builtin").git_status()<CR>]], {noremap = true, silent = true})
