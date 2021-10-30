96f8f31e86a70e3423383fddfd84ea9a1e10b494
Doug Sheffer <desheffer@gmail.com>
2021-10-29 23:32:46 -0400

    Enable hop in more modes

--- home/.config/nvim/lua/plugins/hop.lua
+++ home/.config/nvim/lua/plugins/hop.lua
@@ -1,4 +1,4 @@
 require("hop").setup()
 
-vim.api.nvim_set_keymap("n", "\\", [[<Cmd>HopChar1<CR>]], {noremap = true})
-vim.api.nvim_set_keymap("n", "\\\\", [[<Cmd>HopWord<CR>]], {noremap = true})
+vim.api.nvim_set_keymap("", "\\", [[<Cmd>HopChar1<CR>]], {noremap = true})
+vim.api.nvim_set_keymap("", "\\\\", [[<Cmd>HopWord<CR>]], {noremap = true})
