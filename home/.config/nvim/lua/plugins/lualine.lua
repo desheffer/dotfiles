14049fd2e2bfbd07f68ed3b8b1464560e7bcc78b
Doug Sheffer <desheffer@gmail.com>
2021-10-31 15:19:51 -0400

    Use slant theme

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -1,1 +1,7 @@
-require("lualine").setup()
+require("lualine").setup({
+    options = {
+        disabled_filetypes = {"NvimTree"},
+        section_separators = {left = "", right = ""},
+        component_separators = {left = "", right = ""},
+    },
+})
