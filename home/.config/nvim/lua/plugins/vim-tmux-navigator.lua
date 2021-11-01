c98e832eefe0a2ae22e384dc67b5fc03e28d2e84
Doug Sheffer <desheffer@gmail.com>
2021-10-31 23:42:28 -0400

    Enable vim-tmux-navigator in most modes

--- home/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ home/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,11 +1,13 @@
 vim.g.tmux_navigator_no_mappings = 1
 
-vim.api.nvim_set_keymap("n", "<C-a><Left>", [[<Cmd>TmuxNavigateLeft<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-a><Down>", [[<Cmd>TmuxNavigateDown<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-a><Up>", [[<Cmd>TmuxNavigateUp<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-a><Right>", [[<Cmd>TmuxNavigateRight<CR>]], {noremap = true, silent = true})
+for _, mode in ipairs({"", "c", "i"}) do
+    vim.api.nvim_set_keymap(mode, "<C-a><Left>", [[<Cmd>TmuxNavigateLeft<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-a><Down>", [[<Cmd>TmuxNavigateDown<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-a><Up>", [[<Cmd>TmuxNavigateUp<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-a><Right>", [[<Cmd>TmuxNavigateRight<CR>]], {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<C-a>h", [[<Cmd>TmuxNavigateLeft<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-a>j", [[<Cmd>TmuxNavigateDown<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-a>k", [[<Cmd>TmuxNavigateUp<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-a>l", [[<Cmd>TmuxNavigateRight<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-a>h", [[<Cmd>TmuxNavigateLeft<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-a>j", [[<Cmd>TmuxNavigateDown<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-a>k", [[<Cmd>TmuxNavigateUp<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-a>l", [[<Cmd>TmuxNavigateRight<CR>]], {noremap = true, silent = true})
+end
