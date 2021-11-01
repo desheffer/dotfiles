c80ca6a1dd7f1dc79dc048d79e21d146461a04bb
Doug Sheffer <desheffer@gmail.com>
2021-11-01 16:54:33 -0400

    Automatically create vim dotfiles project

--- init.d/20-nvim.sh
+++ init.d/20-nvim.sh
@@ -2,3 +2,10 @@
 
 # Set up NeoVim.
 nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
+
+# Add dotfiles project.
+PROJECTS="${HOME}/.local/share/nvim/telescope-projects.txt"
+touch "${PROJECTS}"
+if ! grep -q "^dotfiles=" "${PROJECTS}"; then
+    echo "dotfiles=$(pwd)=1" >> "${PROJECTS}"
+fi
