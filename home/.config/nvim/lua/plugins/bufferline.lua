0c86218bdb444976c6db10dd951ea7be0cf32161
Doug Sheffer <desheffer@gmail.com>
2021-11-02 20:56:56 -0400

    Disable close icons the right way

--- home/.config/nvim/lua/plugins/bufferline.lua
+++ home/.config/nvim/lua/plugins/bufferline.lua
@@ -1,13 +1,13 @@
 require("bufferline").setup({
     options = {
-        buffer_close_icon = " ",
-        close_icon = "",
         diagnostics = "nvim_lsp",
         diagnostics_indicator = function(count, level, _, _)
             local icon = level:match("error") and "" or ""
             return " " .. icon .. " " .. count
         end,
         separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
     },
 })
 
