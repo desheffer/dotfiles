8db3571f83a9bda242fcb0fa0e83b1c002f740c7
Doug Sheffer <desheffer@gmail.com>
2021-11-02 23:06:14 -0400

    Add bufferline offset for nvimtree

--- home/.config/nvim/lua/plugins/bufferline.lua
+++ home/.config/nvim/lua/plugins/bufferline.lua
@@ -1,10 +1,12 @@
 require("bufferline").setup({
     options = {
-        diagnostics = "nvim_lsp",
-        diagnostics_indicator = function(count, level, _, _)
-            local icon = level:match("error") and "" or ""
-            return " " .. icon .. " " .. count
-        end,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                highlight = "BufferLineFill",
+                padding = 1,
+            },
+        },
         separator_style = "slant",
         show_buffer_close_icons = false,
         show_close_icon = false,
