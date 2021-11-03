ccaafb3e0b395b7d063bb7ba70d22f7c07f4b2ae
Doug Sheffer <desheffer@gmail.com>
2021-11-03 00:05:14 -0400

    Add vim as a global Lua variable

--- home/.config/nvim/lua/plugins/nvim-lsp-installer.lua
+++ home/.config/nvim/lua/plugins/nvim-lsp-installer.lua
@@ -1,27 +1,40 @@
 local lsp_installer = require("nvim-lsp-installer")
 
+local servers = {
+    bashls = {},
+    cssls = {},
+    dockerls = {},
+    gopls = {},
+    html = {},
+    jdtls = {},
+    jsonls = {},
+    omnisharp = {},
+    pyright = {},
+    sumneko_lua = {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = {"vim"},
+                },
+            },
+        },
+    },
+    yamlls = {},
+}
+
 lsp_installer.on_server_ready(function(server)
-    server:setup({})
+    local opts = {}
+    if servers[server.name] then
+        opts = servers[server.name]
+    end
+
+    server:setup(opts)
     vim.cmd([[do User LspAttachBuffers]])
 end)
 
-local server_names = {
-    "bashls",
-    "cssls",
-    "dockerls",
-    "gopls",
-    "html",
-    "jdtls",
-    "jsonls",
-    "omnisharp",
-    "pyright",
-    "sumneko_lua",
-    "yamlls",
-}
-
 local lsp_installer_servers = require("nvim-lsp-installer.servers")
 
-for _, server_name in ipairs(server_names) do
+for server_name, _ in ipairs(servers) do
     local ok, server = lsp_installer_servers.get_server(server_name)
     if ok and not server:is_installed() then
         server:install()
