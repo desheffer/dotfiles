4ba208fbaa3f9383009301deee17a59d6788010b
Doug Sheffer <desheffer@gmail.com>
2021-11-07 00:09:27 -0400

    Add back LSP formatting keymap

--- home/.config/nvim/lua/plugins/lspconfig.lua
+++ home/.config/nvim/lua/plugins/lspconfig.lua
@@ -10,5 +10,9 @@ vim.api.nvim_set_keymap("n", "gr", [[<Cmd>lua require("telescope.builtin").lsp_r
 
 vim.api.nvim_set_keymap("n", "[d", [[<Cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts = {focusable = false}})<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "]d", [[<Cmd>lua vim.lsp.diagnostic.goto_next({popup_opts = {focusable = false}})<CR>]], {noremap = true, silent = true})
+
 vim.api.nvim_set_keymap("n", "<Leader>ca", [[<Cmd>lua require("telescope.builtin").lsp_code_actions()<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>cf", [[<Cmd>lua vim.lsp.buf.formatting()<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>cr", [[<Cmd>lua vim.lsp.buf.rename()<CR>]], {noremap = true, silent = true})
+
+vim.api.nvim_set_keymap("v", "<Leader>cf", [[<Cmd>lua vim.lsp.buf.range_formatting()<CR>]], {noremap = true, silent = true})
