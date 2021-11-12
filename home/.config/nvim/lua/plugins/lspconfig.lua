1dbf0acad5249aa85673033c52d4e9d42f26bb1c
Doug Sheffer <desheffer@gmail.com>
2021-11-11 21:57:54 -0500

    Tweak code diagnostics keymaps

--- home/.config/nvim/lua/plugins/lspconfig.lua
+++ home/.config/nvim/lua/plugins/lspconfig.lua
@@ -3,15 +3,32 @@ vim.fn.sign_define("LspDiagnosticsSignWarning", {text = ""})
 vim.fn.sign_define("LspDiagnosticsSignInformation", {text = ""})
 vim.fn.sign_define("LspDiagnosticsSignHint", {text = ""})
 
-vim.api.nvim_set_keymap("n", "gd", [[<Cmd>lua require("telescope.builtin").lsp_definitions()<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "gD", [[<Cmd>lua require("telescope.builtin").lsp_declarations()<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "gi", [[<Cmd>lua require("telescope.builtin").lsp_implementations()<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "gr", [[<Cmd>lua require("telescope.builtin").lsp_references()<CR>]], {noremap = true, silent = true})
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, {
+        border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"},
+        focusable = false,
+    }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help, {
+        border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"},
+        focusable = false,
+    }
+)
+
+vim.api.nvim_set_keymap("n", "gd", [[<Cmd>lua require("telescope.builtin").lsp_definitions({jump_type = "never"})<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "gD", [[<Cmd>lua require("telescope.builtin").lsp_declarations({jump_type = "never"})<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "gi", [[<Cmd>lua require("telescope.builtin").lsp_implementations({jump_type = "never"})<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "gr", [[<Cmd>lua require("telescope.builtin").lsp_references({jump_type = "never"})<CR>]], {noremap = true, silent = true})
+
+vim.api.nvim_set_keymap("n", "K", [[<Cmd>lua vim.lsp.buf.hover({focusable = false})<CR>]], {noremap = true, silent = true})
 
 vim.api.nvim_set_keymap("n", "[d", [[<Cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts = {focusable = false}})<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "]d", [[<Cmd>lua vim.lsp.diagnostic.goto_next({popup_opts = {focusable = false}})<CR>]], {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<Leader>ca", [[<Cmd>lua require("telescope.builtin").lsp_code_actions()<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>ca", [[<Cmd>lua require("telescope.builtin").lsp_code_actions(require("telescope.themes").get_cursor())<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>cd", [[<Cmd>lua require("telescope.builtin").lsp_document_diagnostics()<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>cf", [[<Cmd>lua vim.lsp.buf.formatting()<CR>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>cr", [[<Cmd>lua vim.lsp.buf.rename()<CR>]], {noremap = true, silent = true})
 
