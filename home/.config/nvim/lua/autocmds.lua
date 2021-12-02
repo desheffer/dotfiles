e6dd4336ac702cb8bde4b0a88bcd408fb69dfe18
Doug Sheffer <desheffer@gmail.com>
2021-12-02 13:43:47 -0500

    Set colorcolumn dynamically

--- home/.config/nvim/lua/autocmds.lua
+++ home/.config/nvim/lua/autocmds.lua
@@ -7,3 +7,6 @@ vim.cmd([[autocmd FileType gitcommit lua vim.opt.spell = true]])
 
 -- Hide certain buffers in the buffer list.
 vim.cmd([[autocmd FileType qf lua vim.opt.buflisted = false]])
+
+-- Set colorcolumn dynamically if textwidth is set.
+vim.cmd([[autocmd BufEnter * lua if vim.bo.textwidth > 0 then vim.wo.colorcolumn = "+0" end]])
