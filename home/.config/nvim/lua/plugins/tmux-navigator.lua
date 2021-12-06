fca351539fa4422814a2086d311ee5f4b3db3625
Doug Sheffer <desheffer@gmail.com>
2021-12-06 03:24:19 -0500

    Simplify tmux pane navigation bindings

--- home/.config/nvim/lua/plugins/tmux-navigator.lua
+++ home/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,13 +1,8 @@
 vim.g.tmux_navigator_no_mappings = 1
 
 for _, mode in ipairs({"", "c", "i"}) do
-    vim.api.nvim_set_keymap(mode, "<C-a><Left>", [[<Cmd>TmuxNavigateLeft<CR>]], {noremap = true, silent = true})
-    vim.api.nvim_set_keymap(mode, "<C-a><Down>", [[<Cmd>TmuxNavigateDown<CR>]], {noremap = true, silent = true})
-    vim.api.nvim_set_keymap(mode, "<C-a><Up>", [[<Cmd>TmuxNavigateUp<CR>]], {noremap = true, silent = true})
-    vim.api.nvim_set_keymap(mode, "<C-a><Right>", [[<Cmd>TmuxNavigateRight<CR>]], {noremap = true, silent = true})
-
-    vim.api.nvim_set_keymap(mode, "<C-a>h", [[<Cmd>TmuxNavigateLeft<CR>]], {noremap = true, silent = true})
-    vim.api.nvim_set_keymap(mode, "<C-a>j", [[<Cmd>TmuxNavigateDown<CR>]], {noremap = true, silent = true})
-    vim.api.nvim_set_keymap(mode, "<C-a>k", [[<Cmd>TmuxNavigateUp<CR>]], {noremap = true, silent = true})
-    vim.api.nvim_set_keymap(mode, "<C-a>l", [[<Cmd>TmuxNavigateRight<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-w>h", [[<Cmd>TmuxNavigateLeft<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-w>j", [[<Cmd>TmuxNavigateDown<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-w>k", [[<Cmd>TmuxNavigateUp<CR>]], {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, "<C-w>l", [[<Cmd>TmuxNavigateRight<CR>]], {noremap = true, silent = true})
 end
