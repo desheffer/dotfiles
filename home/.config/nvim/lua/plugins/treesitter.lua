4b65e396ca9344eecddbcf91f9404b44143b4713
Doug Sheffer <desheffer@gmail.com>
2022-01-14 16:48:45 -0500

    Enable treesitter incremental selection

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,13 @@ require("nvim-treesitter.configs").setup({
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            node_incremental = ".",
+            node_decremental = ",",
+        },
+    },
 })
 
 function _G.ts_install_sync()
