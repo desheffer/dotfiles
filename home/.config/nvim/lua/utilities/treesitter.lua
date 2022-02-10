6d54ea28f3ed6b5e487034c1a9b419898badbe61
Doug Sheffer <desheffer@gmail.com>
2022-02-10 14:28:39 -0500

    Use a tighter list of Treesitter modules

--- home/.config/nvim/lua/utilities/treesitter.lua
+++ home/.config/nvim/lua/utilities/treesitter.lua
@@ -1,7 +1,34 @@
 _M = {}
 
+_M.languages = {
+    "bash",
+    "c",
+    "c_sharp",
+    "comment",
+    "css",
+    "dockerfile",
+    "go",
+    "gomod",
+    "html",
+    "http",
+    "java",
+    "javascript",
+    "json",
+    "lua",
+    "make",
+    "markdown",
+    "php",
+    "python",
+    "regex",
+    "yaml",
+}
+
 function _M.install_sync()
-    vim.cmd([[TSInstallSync maintained]])
+    local treesitter_install = require("nvim-treesitter.install")
+
+    for _, language_name in ipairs(_M.languages) do
+        treesitter_install.ensure_installed_sync({language_name, with_sync = true})
+    end
 end
 
 return _M
