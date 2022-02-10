51fb52b3538cf9061032cdaf67bce3733db3c018
Doug Sheffer <desheffer@gmail.com>
2022-02-10 02:40:51 -0500

    Remove dashboard icons

--- home/.config/nvim/lua/plugins/dashboard.lua
+++ home/.config/nvim/lua/plugins/dashboard.lua
@@ -1,33 +1,33 @@
 vim.g.dashboard_default_executive = "telescope"
 
 vim.g.dashboard_custom_header = {
-    " ███╗   ██╗ ██╗   ██╗ ██╗ ███╗   ███╗",
-    " ████╗  ██║ ██║   ██║ ██║ ████╗ ████║",
-    " ██╔██╗ ██║ ██║   ██║ ██║ ██╔████╔██║",
-    " ██║╚██╗██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-    " ██║ ╚████║  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-    " ╚═╝  ╚═══╝   ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+    "███╗   ██╗ ██╗   ██╗ ██╗ ███╗   ███╗",
+    "████╗  ██║ ██║   ██║ ██║ ████╗ ████║",
+    "██╔██╗ ██║ ██║   ██║ ██║ ██╔████╔██║",
+    "██║╚██╗██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+    "██║ ╚████║  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+    "╚═╝  ╚═══╝   ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
 }
 
 vim.g.dashboard_custom_section = {
     a = {
-        description = {"ﱐ  New                             <C-n>"},
+        description = {"New                            <C-n>"},
         command = function() vim.cmd([[enew]]) end,
     },
     b = {
-        description = {"  Files                           <C-p>"},
+        description = {"Files                          <C-p>"},
         command = function() require("utilities.finder").find_files() end,
     },
     c = {
-        description = {"﬿  Sessions                    <Space>fs"},
+        description = {"Sessions                   <Space>fs"},
         command = function() require("session-lens").search_session() end,
     },
     d = {
-        description = {"ﮦ  Recent Files                <Space>fo"},
+        description = {"Recent Files               <Space>fo"},
         command = function() require("telescope.builtin").oldfiles() end,
     },
     e = {
-        description = {"  Grep                        <Space>fg"},
+        description = {"Grep                       <Space>fg"},
         command = function() require("telescope.builtin").live_grep() end,
     },
 }
