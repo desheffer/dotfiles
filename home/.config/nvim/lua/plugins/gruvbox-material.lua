0f83ac5c89925cbc1637f2b7ecc4daa0b5125210
Doug Sheffer <desheffer@gmail.com>
2022-02-14 03:39:54 -0500

    Replace FiraCode with JetBrainsMono

--- home/.config/nvim/lua/plugins/gruvbox-material.lua
+++ home/.config/nvim/lua/plugins/gruvbox-material.lua
@@ -4,6 +4,9 @@ vim.opt.background = "dark"
 vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_palette = "original"
 
+vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_enable_italic = 1
+
 vim.cmd([[autocmd ColorScheme gruvbox-material lua require("utilities.colorscheme").gruvbox_material_custom()]])
 
 vim.cmd([[colorscheme gruvbox-material]])
